70861c56b81af3dc6a296e3f3355c846559b56c3
devskale <dev@skale.io>
2025-01-27 13:05:05 +0100

    config.js

---
+++ derchat/config.js
@@ -0,0 +1,15 @@
+// config.js
+
+export const servers = [
+  { url: "https://amp1.mooo.com:11444", description: "ollama" },
+  { url: "https://my-openai-gemini-demo.vercel.app", description: "gemopenai" },
+  { url: "http://pind.mooo.com:11434", description: "ollama" },
+  { url: "http://localhost:11434", description: "ollama" },
+  { url: "https://api.arliai.com", description: "arliopenai" },
+  { url: "http://localhost:8080", description: "llama.cpp" },
+];
+
+export const OPENAI_TOKEN = "%%OPENAI_TOKEN%%"; //
+//export const GEMINI_TOKEN = '%%GEMINI_TOKEN%%'; //
+export const GEMINI_TOKEN = "AIzaSyDPhVmSh_djnUOMP-AYFJ2VsDsV4JiDLGU";
+export const ARLIAI_TOKEN = "5ea1bbdd-7d93-4dbe-b120-2e2a8ee2c2ae";
